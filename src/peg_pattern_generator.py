5fe8369cfe83bc52e187feea23647806510d8abf
Xián <xianmorales01@gmail.com>
2025-12-10 14:03:46 +0100

    refactor(peg_patter_generator): :recycle: Change total amount of colours
    Delete the colors White and Black from the possible pegs that peg_patter_generator can select to create the pattern

--- src/peg_pattern_generator.py
+++ src/peg_pattern_generator.py
@@ -2,7 +2,7 @@ import random
 
 def peg_pattern_generator():
 
-    pegs = ['Red','Cyan','Yellow', 'Purple', 'Green', 'Black', 'White', 'Orange']
+    pegs = ['Red','Cyan','Yellow', 'Purple', 'Green', 'Orange']
     peg_pattern = []
     for _ in range(4):
         peg_pattern.append(random.choice(pegs))
