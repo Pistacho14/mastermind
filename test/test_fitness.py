15ebf98bed5e1dae9e9d5823eb045d56830975a1
Pablo Gonzalez <pablogonzaleztemes@gmail.com>
2025-12-11 09:48:04 +0100

    test(test_fitness): :test_tube: Add new test to test_fitness

--- test/test_fitness.py
+++ test/test_fitness.py
@@ -8,6 +8,7 @@ from src.fitness import calculate_individual_fitness
         (['Red','Black','Yellow','White'], ['Cyan','Green','Purple', 'Orange'], 0),
         (['Red','Green','Yellow','Cyan'], ['Red','Green','Yellow', 'Cyan'], 40),
         (['Red','Black','Purple','Orange'], ['Black','White','Purple', 'Purple'], 15),
+        (['Red','Red','Red','Red'], ['Red','White','Purple', 'Purple'], 10),
 
     ],
 
