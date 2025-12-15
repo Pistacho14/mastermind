0f62ffe4aa6c7a24a40f3b59b869e3382ba284a1
Pablo González <pablogonzaleztemes@gmail.com>
2025-12-15 12:39:26 +0100

    fix(test): :fire: Remove duped files.

--- test/test_first_gen_generator.py
+++
@@ -1,5 +0,0 @@
-from src.first_gen_generator import first_population_generator
-
-def test_length_first_population():
-    population = 150
-    assert len(first_population_generator(population)) == population
