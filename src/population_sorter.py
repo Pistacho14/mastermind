041a3944ff2ed468539d216953bfa3ba835ae2b5
Xián <xianmorales01@gmail.com>
2025-12-16 17:57:24 +0100

    feat(population_sorter): :sparkles: Add new function
    Co-authored-by: Pablo González Pistacho14@users.noreply.github.com

--- src/population_sorter.py
+++ src/population_sorter.py
@@ -1,10 +1,23 @@
 from .check_fitness import check_fitness
 from .fitness_rank_assigner import fitness_rank_assigner
 
-def asd(population, secret_code):
+def population_sorter(population, secret_code):
 
     sorted_population = {'A': [], 'B': [], 'C': [], 'D': [], 'E': [], 'F': []}
     for individual in list(population.keys()):
         fitness = check_fitness(population.get(individual), secret_code)
         fitness_rank_assigner(individual, fitness, sorted_population)
     return sorted_population
+
+print(population_sorter({
+    1: ['Red', 'Green', 'Yellow', 'Cyan'],
+    2: ['Purple', 'Orange', 'Red', 'Green'],
+    3: ['Yellow', 'Yellow', 'Cyan', 'Purple'],
+    4: ['Green', 'Red', 'Orange', 'Orange'],
+    5: ['Cyan', 'Purple', 'Green', 'Red'],
+    6: ['Orange', 'Yellow', 'Red', 'Purple'],
+    7: ['Green', 'Green', 'Cyan', 'Yellow'],
+    8: ['Purple', 'Red', 'Orange', 'Cyan'],
+    9: ['Yellow', 'Green', 'Purple', 'Red'],
+    10: ['Orange', 'Cyan', 'Yellow', 'Green']
+}, ['Green', 'Yellow', 'Red', 'Cyan']))
