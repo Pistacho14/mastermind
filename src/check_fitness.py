58262b40bd8bdbeb4f88b1b17f5a6284b03aabc6
Xián <xianmorales01@gmail.com>
2025-12-16 16:16:14 +0100

    fix(check_fitness): :bug: Correct white peg scoring

--- src/check_fitness.py
+++ src/check_fitness.py
@@ -3,12 +3,21 @@ def check_fitness(peg_pattern, chosen_pegs):
     KEY_PEG_RED = 10
     KEY_PEG_WHITE = 5
     score = 0
+    pegs_remaining = []
+    chosen_pegs_remaining = []
+
     for peg, peg_choose in zip(peg_pattern, chosen_pegs):
-        if peg != peg_choose:
-            continue
-        elif peg == peg_choose:
+        if peg == peg_choose:
             score += KEY_PEG_RED
         else:
+            pegs_remaining.append(peg)
+            chosen_pegs_remaining.append(peg_choose)
+
+    for peg2 in pegs_remaining:
+        if peg2 in chosen_pegs_remaining:
             score += KEY_PEG_WHITE
+            chosen_pegs_remaining.remove(peg2)
+        else:
+            continue
 
     return score
