91c4ebb4acc67282511d5d926c7102d757649630
Xián <xianmorales01@gmail.com>
2025-12-17 18:16:48 +0100

    test(test_roulette_selection): :test_tube: Add a test for roulette_selection

---
+++ test/test_roulette_population.py
@@ -0,0 +1,9 @@
+from src.roulette_selection import roulette_selection
+
+def test_roulette_selection():
+    assert len(roulette_selection({'A': [999],
+                                   'B': [23,24,54,78],
+                                   'C': [15],
+                                   'D': [1],
+                                   'E': [2, 3, 4, 5, 6, 7, 8, 9, 10],
+                                   'F': [324,98,67,56,45,34,21]})) <= 3
