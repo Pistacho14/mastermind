104ccca89c9dd7ae86da70166a72862000543166
Xián <xianmorales01@gmail.com>
2025-12-17 23:29:45 +0100

    refactor(fitness_rank_assigner): :recycle: Delete unreacheable score

--- src/fitness_rank_assigner.py
+++ src/fitness_rank_assigner.py
@@ -1,14 +1,12 @@
 def fitness_rank_assigner(individual, fitness_score, expected_rank):
 
     if fitness_score <= 10:
-        expected_rank['F'].append(individual)
-    elif fitness_score <= 20:
         expected_rank['E'].append(individual)
-    elif fitness_score <= 25:
+    elif fitness_score <= 20:
         expected_rank['D'].append(individual)
-    elif fitness_score <= 30:
+    elif fitness_score <= 25:
         expected_rank['C'].append(individual)
-    elif fitness_score <= 35:
+    elif fitness_score <= 30:
         expected_rank['B'].append(individual)
     else:
         expected_rank['A'].append(individual)
