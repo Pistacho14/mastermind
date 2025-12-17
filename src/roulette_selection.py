c87a7905e748f5ebd6e8849e87421a783ada4324
Xián <xianmorales01@gmail.com>
2025-12-17 23:32:14 +0100

    refactor(roulette_selection): :recycle: Remove unused weight

--- src/roulette_selection.py
+++ src/roulette_selection.py
@@ -3,7 +3,7 @@ import random
 
 def roulette_selection(population_sorted):
     reproducible_population = population_sorted.copy()
-    weights = {'A': 0, 'B': 5, 'C': 10, 'D': 15, 'E': 20, 'F': 25}
+    weights = {'A': 0, 'B': 5, 'C': 10, 'D': 15, 'E': 20}
     valid_ranking = []
     valid_weights = []
     individuals_eliminated = []
@@ -18,12 +18,12 @@ def roulette_selection(population_sorted):
         while len(valid_ranking) > 3:
             eliminado = random.choices(valid_ranking,weights=valid_weights,k=1)[0]
 
-            eliminated_ranking_index = valid_ranking.index(eliminado)
+            eliminated_index = valid_ranking.index(eliminado)
 
             individuals_eliminated.append(eliminado)
 
-            valid_ranking.pop(eliminated_ranking_index)
-            valid_weights.pop(eliminated_ranking_index)
+            valid_ranking.pop(eliminated_index)
+            valid_weights.pop(eliminated_index)
 
 
     for ranking in list(reproducible_population.keys()):
