fd81b3d052c2fb01dfec3c1bd190594a9ee3ffe8
Pablo González <pablogonzaleztemes@gmail.com>
2025-12-18 15:16:48 +0100

    test(test_roulette_population.py): :white_check_mark: Update test for roulette function
    Co-authored-by: Xián Morales <xianmorales01@gmail.com>

--- test/test_roulette_population.py
+++ test/test_roulette_population.py
@@ -1,9 +1,4 @@
 from src.roulette_selection import roulette_selection
 
 def test_roulette_selection():
-    assert len(roulette_selection({'A': [999],
-                                   'B': [23,24,54,78],
-                                   'C': [15],
-                                   'D': [1],
-                                   'E': [2, 3, 4, 5, 6, 7, 8, 9, 10],
-                                   })) <= 3
+    assert len(roulette_selection({'A': [999],'B': [23,24,54,78],'C': [15],'D': [1],'E': [2, 3, 4, 5, 6, 7, 8, 9, 10]})) > 1
