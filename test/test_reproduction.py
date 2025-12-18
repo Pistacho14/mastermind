fd425e781fcca7e52bbc7346c5afeede1f3927aa
Pablo González <pablogonzaleztemes@gmail.com>
2025-12-18 15:14:38 +0100

    test(test_reproduction): :white_check_mark: Update tests

--- test/test_reproduction.py
+++ test/test_reproduction.py
@@ -2,20 +2,24 @@ import pytest
 from src.reproduction import reproduction
 
 @pytest.mark.parametrize(
-    'father, mother, offspring',
+    'father, mother, offspring, counter, expected_offspring',
     [
         (
          ['Red', 'Red', 'Yellow', 'Yellow'],
          ['Yellow', 'Yellow', 'Red', 'Red'],
-         [['Red', 'Red', 'Red', 'Red'], ['Yellow', 'Yellow', 'Yellow', 'Yellow']]
+         {},
+         3,
+         {3: ['Red', 'Red', 'Red', 'Red'], 4: ['Yellow', 'Yellow', 'Yellow', 'Yellow']}
         ),
         (
          ['Cyan', 'Purple', 'Orange', 'Yellow'],
          ['Yellow', 'Green', 'Red', 'Orange'],
-         [['Cyan', 'Purple', 'Red', 'Orange'], ['Yellow', 'Green', 'Orange', 'Yellow']]
+         {},
+         1,
+         {1: ['Cyan', 'Purple', 'Red', 'Orange'], 2: ['Yellow', 'Green', 'Orange', 'Yellow']}
         ),
     ],
 )
 
-def test_reproduction(father, mother, offspring):
-    assert reproduction(father, mother) == offspring
+def test_reproduction(father, mother, offspring, counter, expected_offspring):
+    assert reproduction(father, mother, offspring, counter) == expected_offspring
