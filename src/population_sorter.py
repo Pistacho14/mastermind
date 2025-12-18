143fc5c3654def71cf236a0c375a86a859761db1
Pablo González <pablogonzaleztemes@gmail.com>
2025-12-18 15:19:53 +0100

    refactor(population_sorter): :recycle: remove unintended print
    Co-authored-by: Xián Morales <xianmorales01@gmail.com>

--- src/population_sorter.py
+++ src/population_sorter.py
@@ -8,16 +8,3 @@ def population_sorter(population, secret_code):
         fitness = check_fitness(population.get(individual), secret_code)
         fitness_rank_assigner(individual, fitness, sorted_population)
     return sorted_population
-
-print(population_sorter({
-    1: ['Red', 'Green', 'Yellow', 'Cyan'],
-    2: ['Purple', 'Orange', 'Red', 'Green'],
-    3: ['Yellow', 'Yellow', 'Cyan', 'Purple'],
-    4: ['Green', 'Red', 'Orange', 'Orange'],
-    5: ['Cyan', 'Purple', 'Green', 'Red'],
-    6: ['Orange', 'Yellow', 'Red', 'Purple'],
-    7: ['Green', 'Green', 'Cyan', 'Yellow'],
-    8: ['Purple', 'Red', 'Orange', 'Cyan'],
-    9: ['Yellow', 'Green', 'Purple', 'Red'],
-    10: ['Orange', 'Cyan', 'Yellow', 'Green']
-}, ['Green', 'Yellow', 'Red', 'Cyan']))
