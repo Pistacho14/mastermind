77fade1dfb2f05a3db4089ee4d2698c9a8c5e093
Pablo González <pablogonzaleztemes@gmail.com>
2025-12-18 15:17:24 +0100

    test(test_offspring_generator): :white_check_mark: Add test for new function
    Co-authored-by: Xián Morales <xianmorales01@gmail.com>

---
+++ test/test_offspring_generator.py
@@ -0,0 +1,19 @@
+import pytest
+from src.offspring_generator import offspring_generator
+
+@pytest.mark.parametrize(
+    'reproducible_population, offspring',
+    [
+        (
+         [['Red', 'Red', 'Yellow', 'Yellow'], ['Yellow', 'Yellow', 'Red', 'Red']],
+         {1: ['Yellow', 'Yellow', 'Yellow', 'Yellow'], 2: ['Red', 'Red', 'Red', 'Red']}
+        ),
+        (
+         [['Cyan', 'Purple', 'Orange', 'Yellow'], ['Yellow', 'Green', 'Red', 'Orange']],
+         {1: ['Yellow', 'Green', 'Orange', 'Yellow'], 2: ['Cyan', 'Purple', 'Red', 'Orange']}
+        ),
+    ],
+)
+
+def test_offspring_generator(reproducible_population, offspring):
+    assert sorted(offspring_generator(reproducible_population).values()) == sorted(offspring.values())
