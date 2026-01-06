c89297245edcbe8d72d7372f947a97718ee01ab9
Pablo González <pablogonzaleztemes@gmail.com>
2026-01-06 21:00:56 +0100

    feat(constants): :sparkles: add file with constants
    Co-authored-by: Xián Morales <xianmorales01@gmail.com>

---
+++ src/contants.py
@@ -0,0 +1,16 @@
+COLORS = ['Red','Cyan','Yellow', 'Purple', 'Green', 'Orange']
+
+MUTATION_CHANCE = 0.08
+
+COLOR_CIRCLE = {
+    'Red': "🔴",
+    'Cyan': "🔵",
+    'Green': "🟢",
+    'Yellow': "🟡",
+    'Orange': "🟠",
+    'Purple': "🟣"
+}
+
+KEY_PEG_RED = 10
+KEY_PEG_WHITE = 5
+MAX_TURNS = 14
