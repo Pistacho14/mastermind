6a16bca26f63bafa27f88e3b2329b6c3b17b6504
Pablo González <pablogonzaleztemes@gmail.com>
2026-01-06 06:31:41 +0100

    test(test_mutation): :white_check_mark: add test for new feature mutation

---
+++ test/test_mutation.py
@@ -0,0 +1,24 @@
+import pytest
+from src.mutation import mutation
+
+@pytest.mark.parametrize(
+    '''individuo, 
+    probabilidad_de_mutar, 
+    colores''',
+    [
+        (
+         ['Red', 'Red', 'Yellow', 'Yellow'],
+         1,
+         ['Red','Cyan','Yellow', 'Purple', 'Green', 'Orange'],
+        ),
+        (
+         ['Cyan', 'Purple', 'Orange', 'Yellow'],
+         1,
+         ['Red','Cyan','Yellow', 'Purple', 'Green', 'Orange']
+
+        ),
+    ],
+)
+
+def test_mutation(individuo, probabilidad_de_mutar, colores):
+    assert mutation(individuo, probabilidad_de_mutar, colores) != individuo
