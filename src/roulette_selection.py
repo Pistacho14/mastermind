f9b473e2f8f3c854bb959d399ba80a3f01f8cbf1
Xián <xianmorales01@gmail.com>
2026-01-07 18:03:15 +0100

    feat(roulette_selection): :sparkles: REmake function roulette selection
    Co-authored-by: Pablo González pablogonzaleztemes@gmail.com

--- src/roulette_selection.py
+++ src/roulette_selection.py
@@ -1,29 +1,25 @@
 import random
+from .contants import WEIGHTS, EXPLOTATION_PERCENTAGE
 
 
 def roulette_selection(population_sorted):
-    reproducible_population = population_sorted.copy()
-    weights = {'A': 0, 'B': 2, 'C': 10, 'D': 25, 'E': 35}
-    valid_ranking = []
-    valid_weights = []
-    individuals_eliminated = []
+    population_sorted = population_sorted.copy()
+    individual_reproducible = []
+    individuals_weights = []
+    reproducible_population = {}
+    counter = 1
 
+    for ranking, individuals in population_sorted.items():
+        for individual in individuals:
+            individual_reproducible.append(individual)
+            individuals_weights.append(WEIGHTS[ranking])
 
+    while len(reproducible_population) < EXPLOTATION_PERCENTAGE:
+        selected_individual = random.choices(
+            individual_reproducible, weights=individuals_weights
+        )[0]
 
-    for ranking, individual in reproducible_population.items():
-        if individual:
-            valid_ranking.append(ranking)
-            valid_weights.append(weights[ranking])
+        reproducible_population[counter] = selected_individual.copy()
+        counter += 1
 
-    while len(valid_ranking) > 3:
-        eliminado = random.choices(valid_ranking,weights=valid_weights,k=1)[0]
-
-        eliminated_index = valid_ranking.index(eliminado)
-
-        individuals_eliminated.append(eliminado)
-
-        valid_ranking.pop(eliminated_index)
-        valid_weights.pop(eliminated_index)
-
-
-    return [ind for group in reproducible_population.values() for ind in group]
+    return list(reproducible_population.values())
