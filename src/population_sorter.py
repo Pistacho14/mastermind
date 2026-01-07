aaf81f3dbe24b494c01971e5f4f0b56dfd4ed681
Xián <xianmorales01@gmail.com>
2026-01-07 20:24:21 +0100

    refactor(population_sorter): :recycle: Rename variables
    Co-authored-by: Pablo González pablogonzaleztemes@gmail.com

--- src/population_sorter.py
+++ src/population_sorter.py
@@ -1,10 +1,21 @@
 from .check_fitness import check_fitness
 from .fitness_rank_assigner import fitness_rank_assigner
 
-def population_sorter(population, secret_code):
 
-    sorted_population = {'A': [], 'B': [], 'C': [], 'D': [], 'E': [], 'F': [], 'G': [], 'H': []}
-    for individual in list(population.values()):
-        fitness_score = check_fitness(individual, secret_code)
-        sorted_population = fitness_rank_assigner(individual, fitness_score, sorted_population)
-    return sorted_population
+def population_sorter(current_population, secret_code):
+    ranked_population = {
+        "A": [],
+        "B": [],
+        "C": [],
+        "D": [],
+        "E": [],
+        "F": [],
+        "G": [],
+        "H": [],
+    }
+    for genes in list(current_population.values()):
+        fitness_score = check_fitness(genes, secret_code)
+        ranked_population = fitness_rank_assigner(
+            genes, fitness_score, ranked_population
+        )
+    return ranked_population
