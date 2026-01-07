6b6b27d3c6c237b98450e2c1ed37ddde422b480d
Xián <xianmorales01@gmail.com>
2026-01-07 18:04:24 +0100

    test(test_fitness_rank_assigner): :white_check_mark: Remake tests for fitness_rank_assigner
    Co-authored-by: Pablo González pablogonzaleztemes@gmail.com

--- test/test_fitness_rank_assigner.py
+++ test/test_fitness_rank_assigner.py
@@ -3,55 +3,98 @@ from src.fitness_rank_assigner import fitness_rank_assigner
 
 
 @pytest.mark.parametrize(
-    "individual, fitness_score, population_sorting_system, sorted_population ",
+    "individual, fitness_score, sorted_population, expected_sorted_population ",
     [
-        (1,
-         30,
-         {'A': [],'B': [],'C': [],'D': [],'E': [],},
-         {'A': [],'B': [1],'C': [],'D': [],'E': [],}
-         ),
-
-        (2,
-         10,
-         {'A': [],'B': [],'C': [],'D': [],'E': [],},
-         {'A': [],'B': [],'C': [],'D': [],'E': [2],}
-         ),
-
-        (3,
-         20,
-         {'A': [],'B': [],'C': [],'D': [],'E': []},
-         {'A': [],'B': [],'C': [],'D': [3],'E': []}
-         ),
-
-        (4,
-         0,
-        {'A': [],'B': [],'C': [],'D': [],'E': []},
-        {'A': [],'B': [],'C': [],'D': [],'E': [4]}
+        (
+            ['Yellow', 'Purple', 'Orange', 'Yellow'],
+            30,
+            {
+                "A": [],
+                "B": [],
+                "C": [],
+                "D": [],
+                "E": [],
+                "F": [],
+                "G": [],
+                "H": [],
+            },
+            {
+                "A": [],
+                "B": [['Yellow', 'Purple', 'Orange', 'Yellow']],
+                "C": [],
+                "D": [],
+                "E": [],
+                "F": [],
+                "G": [],
+                "H": [],
+            },
         ),
-
-        (5,
-         30,
-         {'A': [],'B': [],'C': [],'D': [],'E': []},
-         {'A': [],'B': [5],'C': [],'D': [],'E': []}
-         ),
-
-        (6,
-         25,
-         {'A': [],'B': [],'C': [],'D': [],'E': []},
-         {'A': [],'B': [],'C': [6],'D': [],'E': []}
-         ),
-
-        (7,
-         15,
-         {'A': [],'B': [],'C': [],'D': [],'E': []},
-         {'A': [],'B': [],'C': [],'D': [7],'E': []}
-         ),
-
-        (8,
-         40,
-         {'A': [],'B': [],'C': [],'D': [],'E': []},
-         {'A': [8],'B': [],'C': [],'D': [],'E': []}),
+        (
+            ['Cyan', 'Cyan', 'Yellow', 'Orange'],
+            10,
+            {
+                "A": [],
+                "B": [],
+                "C": [],
+                "D": [],
+                "E": [],
+                "F": [],
+                "G": [],
+                "H": [],
+            },
+            {
+                "A": [],
+                "B": [],
+                "C": [],
+                "D": [],
+                "E": [],
+                "F": [['Cyan', 'Cyan', 'Yellow', 'Orange']],
+                "G": [],
+                "H": [],
+            },
+        ),
+        (
+            ['Cyan', 'Red', 'Purple', 'Green'],
+            20,
+            {"A": [], "B": [], "C": [], "D": [], "E": [], "F": [], "G": [], "H": []},
+            {"A": [], "B": [], "C": [], "D": [['Cyan', 'Red', 'Purple', 'Green']], "E": [], "F": [], "G": [], "H": []},
+        ),
+        (
+            ['Red', 'Cyan', 'Purple', 'Red'],
+            0,
+            {"A": [], "B": [], "C": [], "D": [], "E": [], "F": [], "G": [], "H": []},
+            {"A": [], "B": [], "C": [], "D": [], "E": [], "F": [], "G": [], "H": [['Red', 'Cyan', 'Purple', 'Red']]},
+        ),
+        (
+            ['Purple', 'Orange', 'Purple', 'Green'],
+            30,
+            {"A": [], "B": [], "C": [], "D": [], "E": [], "F": [], "G": [], "H": []},
+            {"A": [], "B": [['Purple', 'Orange', 'Purple', 'Green']], "C": [], "D": [], "E": [], "F": [], "G": [], "H": []},
+        ),
+        (
+            ['Cyan', 'Orange', 'Purple', 'Green'],
+            25,
+            {"A": [], "B": [], "C": [], "D": [], "E": [], "F": [], "G": [], "H": []},
+            {"A": [], "B": [], "C": [['Cyan', 'Orange', 'Purple', 'Green']], "D": [], "E": [], "F": [], "G": [], "H": []},
+        ),
+        (
+            ['Red', 'Orange', 'Purple', 'Green'],
+            15,
+            {"A": [], "B": [], "C": [], "D": [], "E": [], "F": [], "G": [], "H": []},
+            {"A": [], "B": [], "C": [], "D": [], "E": [['Red', 'Orange', 'Purple', 'Green']], "F": [], "G": [], "H": []},
+        ),
+        (
+            ['Orange', 'Yellow', 'Purple', 'Orange'],
+            40,
+            {"A": [], "B": [], "C": [], "D": [], "E": [], "F": [], "G": [], "H": []},
+            {"A": [['Orange', 'Yellow', 'Purple', 'Orange']], "B": [], "C": [], "D": [], "E": [], "F": [], "G": [], "H": []},
+        ),
     ],
 )
-def test_population_sorter(individual, fitness_score, population_sorting_system, sorted_population):
-    assert fitness_rank_assigner(individual, fitness_score, population_sorting_system) == sorted_population
+def test_fitness_rank_assigner(
+    individual, fitness_score, sorted_population, expected_sorted_population
+):
+    assert (
+        fitness_rank_assigner(individual, fitness_score, sorted_population)
+        == expected_sorted_population
+    )
