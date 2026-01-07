04fa816b13796027969f2c2843107d0aa1ec3dd0
Xián <xianmorales01@gmail.com>
2026-01-07 20:20:45 +0100

    refactor(fitness_rank_assigner): :recycle: Rename parameter
    Co-authored-by: Pablo González pablogonzaleztemes@gmail.com

--- src/fitness_rank_assigner.py
+++ src/fitness_rank_assigner.py
@@ -1,18 +1,18 @@
-def fitness_rank_assigner(individual, fitness_score, sorted_population):
+def fitness_rank_assigner(individual, fitness_score, ranked_population):
     ranks = [
-        (0, 'H'),
-        (5, 'G'),
-        (10, 'F'),
-        (15, 'E'),
-        (20, 'D'),
-        (25, 'C'),
-        (30, 'B'),
-        (float('inf'), 'A'),
+        (0, "H"),
+        (5, "G"),
+        (10, "F"),
+        (15, "E"),
+        (20, "D"),
+        (25, "C"),
+        (30, "B"),
+        (float("inf"), "A"),
     ]
 
     for limit, rank in ranks:
         if fitness_score <= limit:
-            sorted_population[rank].append(individual)
+            ranked_population[rank].append(individual)
             break
 
-    return sorted_population
+    return ranked_population
