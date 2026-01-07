4b778c01812a9b0b6600ac0b8850e872f1263145
Xián <xianmorales01@gmail.com>
2026-01-07 18:15:29 +0100

    fix(population mixer): :bug: Fix list bug from population mixer
    Co-authored-by: Pablo González pablogonzaleztemes@gmail.com

--- src/population_mixer.py
+++ src/population_mixer.py
@@ -1,25 +1,30 @@
 import random
 from .population_sorter import population_sorter
+from .contants import WEIGHTS
+
 
 def population_mixer(current_population, secret_code, offspring):
-
-    next_gen_patterns = list(offspring.values())
+    next_gen_patterns = [p.copy() for p in offspring.values()]
     next_gen = {}
 
-    WEIGHTS = {'B': 100, 'C': 45, 'D': 30, 'E': 10, 'F': 5, 'G': 3, 'H': 1}
     actual_population_candidates = []
     actual_population_weights = []
 
-    for ranking, individuals in population_sorter(current_population, secret_code).items():
-        for individual_id in individuals:
-            actual_population_candidates.append(individual_id)
+    for ranking, individuals in population_sorter(
+        current_population, secret_code
+    ).items():
+        for individual in individuals:
+            actual_population_candidates.append(individual)
             actual_population_weights.append(WEIGHTS[ranking])
 
     while len(next_gen_patterns) < len(current_population):
-        selected_population = random.choices(actual_population_candidates, weights=actual_population_weights)
-        next_gen_patterns.append(selected_population)
+        selected = random.choices(
+            actual_population_candidates, weights=actual_population_weights
+        )[0]
 
-    for individual, peg_pattern in enumerate(next_gen_patterns, start=1):
-        next_gen[individual] = peg_pattern
+        next_gen_patterns.append(selected.copy())
+
+    for individual_id, peg_pattern in enumerate(next_gen_patterns, start=1):
+        next_gen[individual_id] = peg_pattern
 
     return next_gen
