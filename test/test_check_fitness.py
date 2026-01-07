cb7ad5e401fe266f7ce11fcef74eddf3c34dece6
Xián <xianmorales01@gmail.com>
2026-01-07 20:27:28 +0100

    style(test_check_fitness): :art: Format code
    Co-authored-by: Pablo González pablogonzaleztemes@gmail.com

--- test/test_check_fitness.py
+++ test/test_check_fitness.py
@@ -3,16 +3,13 @@ from src.check_fitness import check_fitness
 
 
 @pytest.mark.parametrize(
-    'peg_pattern,chosen_pegs,score',
+    "peg_pattern,chosen_pegs,score",
     [
-        (['Red','Red','Yellow','Yellow'], ['Cyan','Green','Purple', 'Orange'], 0),
-        (['Red','Green','Yellow','Cyan'], ['Red','Green','Yellow', 'Cyan'], 40),
-        (['Red','Cyan','Purple','Orange'], ['Red','Red','Purple', 'Purple'], 20),
-        (['Red','Red','Red','Red'], ['Red','Cyan','Purple', 'Purple'], 10),
-
+        (["Red", "Red", "Yellow", "Yellow"], ["Cyan", "Green", "Purple", "Orange"], 0),
+        (["Red", "Green", "Yellow", "Cyan"], ["Red", "Green", "Yellow", "Cyan"], 40),
+        (["Red", "Cyan", "Purple", "Orange"], ["Red", "Red", "Purple", "Purple"], 20),
+        (["Red", "Red", "Red", "Red"], ["Red", "Cyan", "Purple", "Purple"], 10),
     ],
-
 )
-
 def test_check_fitness(peg_pattern, chosen_pegs, score):
     assert check_fitness(peg_pattern, chosen_pegs) == score
