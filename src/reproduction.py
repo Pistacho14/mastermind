d7962c103f62be6f89d3fdd1645e2ed46354b8d7
Xián <xianmorales01@gmail.com>
2026-01-07 20:25:38 +0100

    refactor(reproduction): :recycle: Rename parameter and variables
    Co-authored-by: Pablo González pablogonzaleztemes@gmail.com

--- src/reproduction.py
+++ src/reproduction.py
@@ -1,15 +1,14 @@
 from .mutation import mutation
-from .contants import MUTATION_CHANCE, COLORS
 
 
-def reproduction(father, mother, offspring, counter):
+def reproduction(father, mother, offspring, child_id):
     first_child = father[0:2] + mother[2:]
     second_child = mother[0:2] + father[2:]
 
-    first_child = mutation(first_child, MUTATION_CHANCE, COLORS)
-    second_child = mutation(second_child, MUTATION_CHANCE, COLORS)
+    first_child = mutation(first_child)
+    second_child = mutation(second_child)
 
-    offspring[counter] = first_child
-    offspring[counter + 1] = second_child
+    offspring[child_id] = first_child
+    offspring[child_id + 1] = second_child
 
     return offspring
