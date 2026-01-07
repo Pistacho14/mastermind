b745a76480de022ac9dd180b57d7433c6ccc3f30
Xián <xianmorales01@gmail.com>
2026-01-07 17:57:31 +0100

    feat: :sparkles: Add mutation to reproduction function

--- src/reproduction.py
+++ src/reproduction.py
@@ -1,5 +1,15 @@
-def reproduction(father, mother, offspring, counter):
+from .mutation import mutation
+from .contants import MUTATION_CHANCE, COLORS
 
-    offspring[counter] = father[0:2] + mother[2::]
-    offspring[counter + 1] = mother[0:2] + father[2::]
+
+def reproduction(father, mother, offspring, counter):
+    first_child = father[0:2] + mother[2:]
+    second_child = mother[0:2] + father[2:]
+
+    first_child = mutation(first_child, MUTATION_CHANCE, COLORS)
+    second_child = mutation(second_child, MUTATION_CHANCE, COLORS)
+
+    offspring[counter] = first_child
+    offspring[counter + 1] = second_child
+
     return offspring
