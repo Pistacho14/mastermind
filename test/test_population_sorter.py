00677f4036b9f28a79c47794dae290022d715815
Xián <xianmorales01@gmail.com>
2026-01-07 18:05:31 +0100

    test(test_population_sorter): :white_check_mark: Remake tests for population_sorter
    Co-authored-by: Pablo González pablogonzaleztemes@gmail.com

--- test/test_population_sorter.py
+++ test/test_population_sorter.py
@@ -2,40 +2,49 @@ import pytest
 from src.population_sorter import population_sorter
 
 dict1 = {
-    1: ['Red', 'Green', 'Yellow', 'Cyan'],
-    2: ['Purple', 'Orange', 'Red', 'Green'],
-    3: ['Yellow', 'Yellow', 'Cyan', 'Purple'],
-    4: ['Green', 'Red', 'Orange', 'Orange'],
-    5: ['Cyan', 'Purple', 'Green', 'Red'],
-    6: ['Orange', 'Yellow', 'Red', 'Purple'],
-    7: ['Green', 'Green', 'Cyan', 'Yellow'],
-    8: ['Purple', 'Red', 'Orange', 'Cyan'],
-    9: ['Yellow', 'Green', 'Purple', 'Red'],
-    10: ['Orange', 'Cyan', 'Yellow', 'Green']
+    1: ["Red", "Green", "Yellow", "Cyan"],
+    2: ["Purple", "Orange", "Red", "Green"],
+    3: ["Yellow", "Yellow", "Cyan", "Purple"],
+    4: ["Green", "Red", "Orange", "Orange"],
+    5: ["Cyan", "Purple", "Green", "Red"],
+    6: ["Orange", "Yellow", "Red", "Purple"],
+    7: ["Green", "Green", "Cyan", "Yellow"],
+    8: ["Purple", "Red", "Orange", "Cyan"],
+    9: ["Yellow", "Green", "Purple", "Red"],
+    10: ["Orange", "Cyan", "Yellow", "Green"]
 }
 
+
 @pytest.mark.parametrize(
-    'population, secret_code, sorted_population',
+    "population, secret_code, sorted_population",
     [
         (
-         dict1,
-         ['Green', 'Yellow', 'Red', 'Cyan'],
-         {'A': [],
-          'B': [],
-          'C': [['Red', 'Green', 'Yellow', 'Cyan']],
-          'D': [['Purple', 'Orange', 'Red', 'Green'],
-                ['Yellow', 'Yellow', 'Cyan', 'Purple'],
-                ['Green', 'Red', 'Orange', 'Orange'],
-                ['Cyan', 'Purple', 'Green', 'Red'],
-                ['Orange', 'Yellow', 'Red', 'Purple'],
-                ['Green', 'Green', 'Cyan', 'Yellow'],
-                ['Purple', 'Red', 'Orange', 'Cyan'],
-                ['Yellow', 'Green', 'Purple', 'Red'],
-                ['Orange', 'Cyan', 'Yellow', 'Green']],
-          'E': []}
+            dict1,
+            ["Green", "Red", "Purple", "Cyan"],
+            {
+                "A": [],
+                "B": [],
+                "C": [["Purple", "Red", "Orange", "Cyan"]],
+                "D": [
+                    ["Red", "Green", "Yellow", "Cyan"],
+                    ["Green", "Red", "Orange", "Orange"],
+                    ["Cyan", "Purple", "Green", "Red"],
+                    ["Yellow", "Green", "Purple", "Red"],
+                ],
+                "E": [
+                    ["Purple", "Orange", "Red", "Green"],
+                    ["Green", "Green", "Cyan", "Yellow"],
+                ],
+                "F": [
+                    ["Yellow", "Yellow", "Cyan", "Purple"],
+                    ["Orange", "Yellow", "Red", "Purple"],
+                    ["Orange", "Cyan", "Yellow", "Green"],
+                ],
+                "G":[],
+                "H": [],
+            },
         ),
     ],
 )
-
 def test_population_sorter(population, secret_code, sorted_population):
     assert population_sorter(population, secret_code) == sorted_population
