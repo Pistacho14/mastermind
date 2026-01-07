8e525753f1a4197f4d18e53f8dcdedea9929b154
Xián <xianmorales01@gmail.com>
2026-01-07 20:28:40 +0100

    refactor(test_first_population_generator): :recycle: Change parameter and variable
    Co-authored-by: Pablo González pablogonzaleztemes@gmail.com

--- test/test_first_population_generator.py
+++ test/test_first_population_generator.py
@@ -1,5 +1,6 @@
-from src.first_population_generator import first_population_generator
+from src.first_population_generator import initial_population_generator
+
 
 def test_length_first_population():
-    population = 150
-    assert len(first_population_generator(population)) == population
+    population = 100
+    assert len(initial_population_generator()) == population
