6260902e9265aed41cb7bcfaaeb01cae07e3df8a
Xián <xianmorales01@gmail.com>
2026-01-07 20:22:23 +0100

    refactor(offspring_generator): :recycle: Renam,e parameter and variables
    Co-authored-by: Pablo González pablogonzaleztemes@gmail.com

--- src/offspring_generator.py
+++ src/offspring_generator.py
@@ -1,16 +1,16 @@
 import random
 from .reproduction import reproduction
 
-def offspring_generator(reproducible_population):
 
-    population = reproducible_population.copy()
+def offspring_generator(selected_population):
+    selected_population = selected_population.copy()
     offspring = {}
-    counter = 1
-    while len(population) > 1:
-        parents = random.sample(population, 2)
+    child_id = 1
+    while len(selected_population) > 1:
+        parents = random.sample(selected_population, 2)
         father, mother = parents
-        offspring = reproduction(father,mother, offspring, counter)
-        population.remove(father)
-        population.remove(mother)
-        counter += 2
-    return  offspring
+        offspring = reproduction(father, mother, offspring, child_id)
+        selected_population.remove(father)
+        selected_population.remove(mother)
+        child_id += 2
+    return offspring
